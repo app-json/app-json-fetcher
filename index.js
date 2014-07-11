e85761a2191280e1c57aa59e8ea5e80662fa49fa
zeke <zeke@sikelianos.com>
2014-07-10 20:17:43 -0700

    simple webservice with support bitbucket and github app.json fetching

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
 "use strict"
 
-require("dotenv").load()
+var util = require("util")
+var superagent = require("superagent")
+var github = require("github-url-to-object")
+var bitbucket = require("bitbucket-url-to-object")
+var logfmt  = require("logfmt")
+var express = require("express")
+var app = module.exports = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(logfmt.bodyParserStream())
+
+app.get('/', function (req, res) {
+  var repository = req.query.repository
+  var manifest_url
+  var repo
+
+  if (github(repository)) {
+    repo = github(repository)
+    manifest_url = util.format(
+      "https://api.github.com/repos/%s/%s/contents/app.json?ref=%s",
+      repo.user,
+      repo.repo,
+      repo.branch
+    )
+
+  } else if (bitbucket(repository)) {
+    repo = bitbucket(repository)
+    manifest_url = util.format(
+      "https://bitbucket.org/%s/%s/raw/%s/app.json",
+      repo.user,
+      repo.repo,
+      repo.branch
+    )
+
+  } else {
+    return res.send(500, {error: "Github or Bitbucket URL required"})
+  }
+
+  if (process.env.NODE_ENV !== "test")
+    logfmt.log({
+      repository: repository,
+      manifest_url: manifest_url
+    })
+
+  superagent.get(manifest_url, function(err, res2) {
+    var manifest
+
+    if (err)
+      return res.send(500, {error: err})
+
+    if (res2.status == 404)
+      return res.send(res2.status, {error: "Not found: " + manifest_url})
+
+    if (res2.status !== 200)
+      return res.send(res2.status, res2.body)
+
+    if (res2.body.content) {
+      // Github
+      manifest = new Buffer(res2.body.content, 'base64').toString()
+    } else {
+      // Bitbucket
+      manifest = res2.text
+    }
+
+    res.json(JSON.parse(manifest))
+  })
+
+})
+
+
+if (!process.parent)
+  app.listen(app.get('port'))
--- test/index.js
+++ test/index.js
@@ -1,22 +1,60 @@
 "use strict"
 
-require("dotenv").load()
-var assert = require("assert")
+process.env.NODE_ENV = "test"
 
-describe("thing", function() {
+// var assert = require("assert")
+var supertest = require("supertest")
+var app = require("..")
 
-  beforeEach(function() {
-    // stuff
+
+describe('GET /', function(){
+  it('fetches app.json content from GitHub', function(done){
+    supertest(app)
+      .get('/')
+      .query({repository: "github:zeke/slideshow"})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) throw err
+        done()
+      })
   })
 
-  it("does stuff", function() {
-    assert(true)
+  it('fetches app.json content from Bitbucket', function(done){
+    supertest(app)
+      .get('/')
+      .query({repository: "bitbucket:sikelianos/web-starter-kit"})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res){
+        if (err) throw err
+        done()
+      })
   })
 
-  it("reads environment from .env file", function() {
-    assert.equal(process.env.FOO, "BAR")
+  it('accepts deep URLs', function(done){
+    supertest(app)
+      .get('/')
+      .query({repository: "https://github.com/zeke/slideshow/blob/master/app.json"})
+      .expect('Content-Type', /json/)
+      .expect(200, done)
   })
 
-  it("has pending stuff")
+  it('returns a 404 if file is not found', function(done){
+    supertest(app)
+      .get('/')
+      .query({repository: "bitbucket:sikelianos/nonexistent"})
+      .expect('Content-Type', /json/)
+      .expect(404, done)
+  })
+
+  it('returns a 404 if file is not found', function(done){
+    supertest(app)
+      .get('/')
+      .query({repository: "github:zeke/nonexistent"})
+      .expect('Content-Type', /json/)
+      .expect(404, done)
+  })
+
 
 })
