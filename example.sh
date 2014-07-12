dacf113122fc182d730c4dcfe7217f0a282e1bef
zeke <zeke@sikelianos.com>
2014-07-11 21:59:28 -0700

    update readme

--- example.sh
+++
@@ -1,8 +0,0 @@
-# Github shorthand
-curl https://app-json-fetcher.herokuapp.com/?repository=github:zeke/slideshow
-
-# Bitbucket shorthand
-curl https://app-json-fetcher.herokuapp.com/?repository=bitbucket:sikelianos/web-starter-kit
-
-# Long URLs
-curl https://app-json-fetcher.herokuapp.com/?repository=https://github.com/zeke/slideshow/tree/master/app.json
